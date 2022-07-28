ea4e1c085b9bf33612c8fc24a0e837b0cb09d054
rhakta <rhakta@gmail.com>
2022-07-28 21:17:42 +0200

    Commit at the moment

--- public/GitPull.bat
+++ public/GitPull.bat
@@ -2,7 +2,6 @@
 title Git Pull
 echo Debut de l'execution des commandes.
 cd ..
-git clone https://github.com/rhakta/PrototypeLTE.git
 git add *
 git commit -am "Commit at the moment"
 git pull https://github.com/rhakta/PrototypeLTE.git
